2c6564d1881b275b3f522d51f0615ebf5b18c23b
MsterKurdi <kurdimr433@gmail.com>
2024-08-16 02:42:21 +0300

    Add files via upload

---
+++ Travel_WEBSITE/app.js
@@ -0,0 +1,30 @@
+const btncall= document.querySelector(".btn");
+const btn= document.querySelector(".btn1");
+
+const body= document.querySelector("body");
+const h1= document.querySelector("h1");
+const h2= document.querySelector("h2");
+
+btncall.addEventListener("click" , ()=>{
+    h1.style.color="black";
+    body.style.background="black";
+    
+    });
+    btn.addEventListener("click" , ()=>{
+        h1.style.color="white";
+        body.style.background="white";
+     
+        });
+    //     btncall.addEventListener("click" , ()=>{
+    //         h1.style.color="white";
+    //         body.style.background="white";
+            
+    //         });
+    //         btn.addEventListener("click" , ()=>{
+    //             h2.style.color="black";
+    //             body.style.background="black";
+                
+    //             });
+
+            
+  
